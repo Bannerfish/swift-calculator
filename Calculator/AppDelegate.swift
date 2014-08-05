d58923eb725a2301ed1a3093baeddfd0f056430b
Jan Andersson <jan.andersson@gmail.com>
2014-08-05 15:45:31 +0200

    Initial Commit

---
+++ Calculator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Calculator
+//
+//  Created by Jan Andersson on 2014-08-05.
+//  Copyright (c) 2014 Jan Andersson. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Calculator/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Calculator
+//
+//  Created by Jan Andersson on 2014-08-05.
+//  Copyright (c) 2014 Jan Andersson. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
